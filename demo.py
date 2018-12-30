db364e66047b5f0596cdd37c6ae2ddf53033019c
Clemens Korndörfer <cknd@mailbox.org>
2018-12-30 14:09:58 +0100

    cleanup

--- demo.py
+++ demo.py
@@ -8,18 +8,18 @@ if __name__ == '__main__':
     import stackprinter
     from traceprinter import trace
 
+
     class whatever():
         # @trace(style='color')#, blacklist=[r'site-packages/numpy'])
         def ahoi(self):
-            # raise Exception()
             return some_function('')
 
     blub = '123'
     somelist = [1,2,3,np.zeros((23,42)), np.ones((42,23))]
-    outer_scope_thing = {'various': 'excellent',
-                         123: 'things',
-                         'in': np.ones((42,23)),
-                         'a list': somelist}
+    outer_scope_thing = {'various': 'things',
+                         123: 'in a dict',
+                         'and': np.ones((42,23)),
+                         'in a list': somelist}
 
     def deco(f):
         bla = 1234
@@ -41,19 +41,16 @@ if __name__ == '__main__':
         bla = """ another triple quoted string """
         X = np.zeros((10,10))
         X[0,0] = 1
-        boing = outer_scope_thing['in']
+        boing = outer_scope_thing['and']
         foo = somelist
-        do_something = lambda val: another_broken_function(val + outer_scope_thing['in'] + boing)
+        do_something = lambda val: another_broken_function(val + outer_scope_thing['and'] + boing)
         for k in X:
             if np.sum(k) != 0:
-                # raise Exception()
-                hiho = locals()
-                listcomp = [[do_something(val) for val in row] for row in X]
+                return [[do_something(val) for val in row] for row in X]
 
     def some_function(boing, zap='!'):
         thing = boing + zap
         a_broken_function(thing)
-
 
     def another_broken_function(val):
         if np.sum(val) != 42:
@@ -66,20 +63,13 @@ if __name__ == '__main__':
             ahoi = "here is a "\
                    "multi line "\
                    "string"
-
-            # stackprinter.show_stack(style='color')
             np.reshape(bla, 9000)
             boing = np.\
                     random.rand(*bla.T.\
                             T.T)
             raise Exception('something happened')
 
-
-
-
     try:
-        wha = whatever()
-        wha.ahoi()
+        whatever().ahoi()
     except:
-        pass
         stackprinter.show(style='color', source_lines=5, reverse=False, truncate_vals=500, suppressed_paths=["site-packages"])
