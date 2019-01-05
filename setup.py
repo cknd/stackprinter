4806d7bc11e545fef90e8af75f3906fb5ec7f04e
Clemens Korndörfer <cknd@mailbox.org>
2019-01-05 19:40:49 +0100

    update readme

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stackprinter",
-    version="0.1",
+    version="0.1.2",
     author="cknd",
     author_email="ck-github@mailbox.org",
-    description="Print more detailed call stacks, with current variable values etc",
+    description="Prettier tracebacks & call stacks with variable values and colorful highlighting",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cknd/stackprinter",
