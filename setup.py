d62bf7d786d625d10a7e641ac7a4dedc99cfc438
Clemens Korndörfer <cknd@mailbox.org>
2019-04-29 00:02:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
+    python_requires=">=3.4",
     name="stackprinter",
     version="0.1.6",
     author="cknd",
