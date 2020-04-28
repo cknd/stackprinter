49a44cdc1e724a7e57b6c280951f9dd10555a1e6
spacemanspiff2007 <accounts@sgerber.de>
2020-04-28 07:21:35 +0200

    Allow passing of compiled regexes as ignore patterns

--- stackprinter/utils.py
+++ stackprinter/utils.py
@@ -11,7 +11,13 @@ def match(string, patterns):
         patterns = [patterns]
     elif patterns is None:
         return False
-    return any([bool(re.search(p, string)) for p in patterns])
+    
+    # compile the list of strings to a pattern
+    for i, p in patterns:
+        if isinstance(p, str):
+            patterns[i] = re.compile(p)
+    
+    return any([bool(p.search(string)) for p in patterns])
 
 def inspect_callable(f):
     """
