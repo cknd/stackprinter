dd634e123f4570c55553d6ec6614564f394d34f8
cknd <clemens@micropsi-industries.com>
2023-11-24 21:16:12 +0100

    Exclude the exceptiongroups demo from linting since the name ExceptionGroup is undefined in older pythons

--- demo_exceptiongroups.py
+++ demo_exceptiongroups.py
@@ -1,6 +1,7 @@
+# flake8: noqa
+
 # Exception groups (new in py 3.11) aren't supported so far,
 # but at least we fall back on the default message.
-
 
 import stackprinter
 stackprinter.set_excepthook()
