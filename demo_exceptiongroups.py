b1356bdc851d5425d8485f49ad7eab5bc77925b3
cknd <clemens@micropsi-industries.com>
2023-11-24 21:12:36 +0100

    Add demo of exception group behavior

---
+++ demo_exceptiongroups.py
@@ -0,0 +1,15 @@
+# Exception groups (new in py 3.11) aren't supported so far,
+# but at least we fall back on the default message.
+
+
+import stackprinter
+stackprinter.set_excepthook()
+
+
+def raise_group():
+    group = ExceptionGroup('A group!',
+        [Exception("Something!"), Exception("Something else!")])
+    raise group
+
+
+raise_group()
